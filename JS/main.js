b519d520176af2db3041780010c76c2cdc994efe
NelsonVillarreal <devnelsonvillarreal@gmail.com>
2022-11-02 21:01:45 -0400

    Organizaçao Js

---
+++ JS/main.js
@@ -0,0 +1,23 @@
+const navMenu = document.getElementById('nav-menu');
+
+/* ----- MENU SHOW -----*/
+/* encaixar en una ventanita el menu
+
+falta colocar el navClose
+ Buscar como remover el menu en el telefono
+
+ efectos para la seccion de skills tipo acordeon
+
+ paginacion swiper para los proyectos
+
+ efecto para el scroll
+  
+ efecto claro y oscuro
+
+ DIOSSSS MIO me voy a volver locooo dame paciencia senor
+
+/* ----- MENU HIDDEN -----*/
+/* Recuerda colocar efecto desaparecer menu */
+
+
+
